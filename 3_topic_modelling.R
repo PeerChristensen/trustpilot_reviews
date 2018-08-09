f92d526fa14922f66238c1c1b8f71cc805611504
PeerChristensen <hr.pchristensen@gmail.com>
2018-08-09 12:51:56 +0200

    add word correlations

--- 3_topic_modelling.R
+++ 3_topic_modelling.R
@@ -10,6 +10,9 @@ library(magrittr)
 library(happyorsad)
 library(jtools)
 library(gridExtra)
+library(widyr)
+library(igraph)
+library(ggraph)
 
 # SET THEME
 my_theme <- function() {
@@ -149,9 +152,64 @@ plot_neg <- topTerms_neg %>%
   my_theme() +
   theme(axis.title=element_blank())
 
+grid.arrange(plot_pos,plot_neg, ncol = 1)
+
+# for saving as image
 combined_plot = arrangeGrob(plot_pos,plot_neg, ncol = 1)
 combined_plot
 ggsave("combined_plot.png", combined_plot, height = 10, width = 12)
+
+# Pairwise word co-occurence
+word_pairs_pos <- df_pos %>% 
+  pairwise_count(word, Name, sort = TRUE)
+
+word_pairs_neg <- df_neg %>%
+  pairwise_count(word, Name, sort = TRUE)
+
+# plots
+set.seed(611)
+
+pairs_plot_pos <- word_pairs_pos %>%
+  filter(n >= 140) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "steelblue") +
+  ggtitle("Positive word pairs") +
+  geom_node_point(size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                point.padding = unit(0.2, "lines")) +
+  my_theme() +
+  theme(axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank())
+
+pairs_plot_neg <- word_pairs_neg %>%
+  filter(n >= 80) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "indianred") +
+  ggtitle("Negative word pairs") +
+  geom_node_point(size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                 point.padding = unit(0.2, "lines")) +
+  my_theme() +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
+
+grid.arrange(pairs_plot_pos, pairs_plot_neg, ncol = 2)
+
+
+# word pair correlations
+df_pos %>% group_by(word) %>%
+  filter(n() >= 100) %>%
+  pairwise_cor(word, Name, sort = TRUE) %>% filter(item1 == "god") %>%
+  top_n(7)
+
+df_neg %>% group_by(word) %>%
+  filter(n() >= 100) %>%
+  pairwise_cor(word, Name, sort = TRUE) %>% filter(item1 == "dårlig") %>%
+  top_n(7)
 
 # sentiment score, try with map()
 #sentiment = NULL
